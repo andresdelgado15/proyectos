9befada38e8e8807dee00f20c83f5fb76b8dd974
Andres Delgado <andresd.7878@hotmail.com>
2023-04-08 02:08:06 +1000

    actualizacion CV

---
+++ calculadora/js/script.js
@@ -0,0 +1,18 @@
+function agregar(valor) {
+	var resultado = document.getElementById('resultado');
+	resultado.value += valor;
+}
+
+function borrar() {
+	var resultado = document.getElementById('resultado');
+	resultado.value = '';
+}
+
+function calcular() {
+	var resultado = document.getElementById('resultado');
+	try {
+		resultado.value = eval(resultado.value);
+	} catch (error) {
+		resultado.value = 'Error';
+	}
+}
