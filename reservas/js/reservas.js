3f8ee2b2be5ef9631eeb3ac3ddc52df0be04f44b
Andres Delgado <andresd.7878@hotmail.com>
2023-04-10 02:26:40 +1000

    actualizacion proyecto reserva

--- reservas/js/reservas.js
+++ reservas/js/reservas.js
@@ -10,15 +10,6 @@ $(document).ready(function () {
   let reservedDates = [];
   $("#reservationForm")[0].reset();
 
-  $("#date, #time").on("change", function () {
-    const selectedDate = $("#date").val();
-    const selectedTime = $("#time").val();
-    if (isReservationAvailable(selectedDate, selectedTime)) {
-      $("#date, #time").removeClass("reserved-field");
-    } else {
-      $("#date, #time").addClass("reserved-field");
-    }
-  });
   // Configurar Toastr.js
   toastr.options = {
     closeButton: true,
@@ -29,17 +20,6 @@ $(document).ready(function () {
     timeOut: "2000", // Duración del mensaje en milisegundos
     extendedTimeOut: "1000",
   };
-  $("#date").datepicker({
-    dateFormat: "yy-mm-dd",
-    minDate: 0,
-    beforeShowDay: function (date) {
-      const dateString = $.datepicker.formatDate("yy-mm-dd", date);
-      if (reservedDates.includes(dateString)) {
-        return [false, "reserved-date", "Reservado"]; // Colorear la fecha en rojo y mostrar un mensaje de "Reservado" en el tooltip
-      }
-      return [true, ""]; // Mantener el estilo predeterminado para las fechas disponibles
-    },
-  });
 
   $("#reservationForm").submit(function (event) {
     event.preventDefault();
@@ -62,32 +42,30 @@ $(document).ready(function () {
       const randomNumber = Math.floor(Math.random() * imageNames.length);
       return `./img/${imageNames[randomNumber]}`;
     }
+    const reservationInfo = isReservationAvailable(date, time);
 
-    if (isReservationAvailable(date, time)) {
-      // Código para agregar la nueva reserva
+    if (!reservationInfo.isReserved) {
       reservations.push({ date, time });
 
       const reservationData = `
-<div class="col-12 col-md-6 col-lg-4 mt-4">
-  <div class="card">
-    <img src="${getRandomImage()}" alt="Imagen aleatoria" style="width: 100%; height: auto; object-fit: cover; border-top-left-radius: 5px; border-top-right-radius: 5px;" />
-    <div class="card-header">
-      Reserva
-    </div>
-    <div class="card-body">
-      <p>
+      <div class="col-12 col-md-6 col-lg-4 mt-4">
+      <div class="card">
+      <img src="${getRandomImage()}" alt="Imagen aleatoria" style="width: 100%; height: auto; object-fit: cover; border-top-left-radius: 5px; border-top-right-radius: 5px;" />
+      <div class="card-header">
+        Reserva
+      </div>
+      <div class="card-body">
+        <p>
         Nombre: ${name}<br>
         Teléfono: ${phone}<br>
-        <span class="selected-date-time">Fecha: ${date}</span><br>
-        <span class="selected-date-time">Hora: ${time}</span><br>
+        Fecha: <span class="dateTime">${date}</span><br>
+        Hora: <span class="dateTime">${time}</span><br>
         Número de personas: ${numPeople}
-      </p>
-    </div>
-  </div>
-</div>
-`;
-  // Agregar la clase selected-time a la opción de hora seleccionada
-  $("#time option[value='" + time + "']").addClass("selected-time");
+        </p>
+      </div>
+      </div>
+      </div>
+      `;
       $("#reservationData").append(reservationData);
       $("#reservationDataContainer").show();
 
@@ -95,24 +73,55 @@ $(document).ready(function () {
       toastr.success("¡Reserva realizada con éxito!");
     } else {
       // Mostrar mensaje de error
-      toastr.error("Ya existe una reserva para la fecha y hora seleccionadas o la fecha y hora ya han pasado.");
+      toastr.error(
+        "Ya existe una reserva para la fecha y hora seleccionadas o la fecha y hora ya han pasado."
+      );
+      // Cambiar el color de la hora reservada a rojo
+      const timeIndex = $("#time option").index(
+        $(`#time option[value='${reservationInfo.time}']`)
+      );
+      $("#time option").eq(timeIndex).css("color", "red");
+
+      // Cambiar el color de la fecha reservada a rojo en el calendario Datepicker
+      if (!reservedDates.includes(reservationInfo.date)) {
+        reservedDates.push(reservationInfo.date);
+        $("#date").datepicker("refresh");
+      }
     }
-   
+  });
+  $("#date").datepicker({
+    dateFormat: "yy-mm-dd",
+    minDate: 0,
+    beforeShowDay: function (date) {
+      const dateString = $.datepicker.formatDate("yy-mm-dd", date);
+      if (reservedDates.includes(dateString)) {
+        return [false, "reserved-date", "Reservado"]; // Colorear la fecha en rojo y mostrar un mensaje de "Reservado" en el tooltip
+      }
+      return [true, ""]; // Mantener el estilo predeterminado para las fechas disponibles
+    },
   });
 
   function isReservationAvailable(date, time) {
     const currentTime = new Date();
     const reservationDateTime = new Date(date + "T" + time);
+
     if (reservationDateTime <= currentTime) {
-      return false;
+      return { isReserved: true };
     }
-    const isReserved = reservations.some(
-      (reservation) => reservation.date === date && reservation.time === time
-    );
-    if (isReserved) {
-      reservedDates.push(date);
-    }
-    return !isReserved;
+    const reservedInfo = {
+      isReserved: false,
+      date: "",
+      time: "",
+    };
+
+    reservations.forEach((reservation) => {
+      if (reservation.date === date && reservation.time === time) {
+        reservedInfo.isReserved = true;
+        reservedInfo.date = date;
+        reservedInfo.time = time;
+      }
+    });
+
+    return reservedInfo;
   }
-  
 });
